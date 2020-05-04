409dcfcba3b320c2c68a261df999cc30c1ca61aa
Sandy <sandy.jernigan@gmail.com>
2020-05-03 23:39:17 -0400

    Still having trouble removing visited and adding to completed

--- adv.py
+++ adv.py
@@ -28,11 +28,8 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 traversal_path = []
-current_path = []
-
-# Rooms to visit
-rooms_to_visit = Queue()
-rooms_to_visit.enqueue(player.current_room.id)
+rooms_to_visit = []
+rooms_to_visit.append(player.current_room.id)
 
 # Rooms Completed
 rooms_completed = []
@@ -41,33 +38,81 @@ rooms_completed = []
 traversal_graph = {}
 room_connections = {}
 
+i = 1
+
 # while the plan_to_visit queue is not Empty:
-while rooms_to_visit.size() > 0:
-    current_path.append(player.current_room.id)
-    # Check if room is in the Traversal Graph
-    if not player.current_room.id in traversal_graph.keys():
-        # If not in graph, then check room and add to graph
-        room_connections = {}
-        # Add each connection as unknown
-        for room in player.current_room.get_exits():
-            room_connections.update({room: "?"})
-        # Add connections to graph
-        traversal_graph.update({player.current_room.id: room_connections})
+#while len(rooms_to_visit) > 0:
+while i < 5:
+    i += 1
+    print (f" Current Room: {player.current_room.id}")
+
+    # Check if in completed list
+    if player.current_room.id not in rooms_completed:
+
+        # Check if room is in the Traversal Graph
+        if not player.current_room.id in traversal_graph.keys():
+            # If not in graph, then check room and add to graph
+            room_connections = {}
+            # Add each connection as unknown
+            for each in player.current_room.get_exits():
+                room_connections.update({each: "?"})
+            # Add connections to graph
+            traversal_graph.update({player.current_room.id: room_connections})
+        
+        # Check Connected Rooms in Graph
+        room_connections = traversal_graph.get(player.current_room.id)
+        print(traversal_graph)
+
+        # Count how many ?
+        # More than 1 means we need to come back
+        if list(room_connections.values()).count("?") > 1:
+            # Add to list to visit
+            if player.current_room.id not in rooms_to_visit:
+                rooms_to_visit.append(player.current_room.id)
+        # More Than 0, we need to check room
+        if list(room_connections.values()).count("?") > 0:
+            # Go to next Room
+            direction = next(key for key, value in room_connections.items() if value == "?")
+            print (direction)
+            # Set Last Room, before moving
+            last_room = player.current_room.id
+            # Move to new Room
+            traversal_path.append(direction)
+            player.travel(direction)
+            # Update Connection
+            room_connections[direction] = player.current_room.id
+            # Find room
+            # for direction in room_connections:
+            #     if room_connections.get(direction) == "?":
+            #         # Set Last Room, before moving
+            #         last_room = player.current_room.id
+            #         # Move to new Room
+            #         traversal_path.append(direction)
+            #         player.travel(direction)
+            #         # Update Connection
+            #         room_connections[direction] = player.current_room.id
+            #         # Leave For Loop
+            #         break
+        
+            #     else:
+            #         print (f"No New Connections: {rooms_to_visit}")
+            #         rooms_completed.append(player.current_room.id)
     else:
-        # If in graph, need to check if any connections are ?
-        print ("In graph")
-        # Remove room if all paths are checked        
-        player.current_room.id = rooms_to_visit.dequeue()
-    
-    room_connections = traversal_graph.get(player.current_room.id)
-    print (f"Room: {room_connections}")
+        # If Room listed as completed, go back
+        if player.current_room.id in rooms_to_visit:
+            rooms_to_visit.remove(player.current_room.id)
+        print (f"Completed: {player.current_room.id}")
+        for direction, room in traversal_graph.get(player.current_room.id).items():
+            print(f"Last Room: {room} {direction}")
+            if room == last_room:
+                traversal_path.append(direction)
+                player.travel(direction)
+                break
 
+print (f"Path: {traversal_path}")
 print (traversal_graph)
-print (current_path)
-
-            # # Add to Rooms to Visit
-            # if room not in rooms_completed:
-            #     rooms_to_visit.enqueue(room)
+print (f"Rooms to Visit: {rooms_to_visit}")
+print (f"Rooms Completed: {rooms_completed}")
 
 
 # # TRAVERSAL TEST - DO NOT MODIFY
@@ -90,12 +135,12 @@ print (current_path)
 # #######
 # # UNCOMMENT TO WALK AROUND
 # #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
