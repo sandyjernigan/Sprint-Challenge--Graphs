8ee143afbe7f652b438f67ad6deba928331046b2
Sandy <sandy.jernigan@gmail.com>
2020-05-04 20:08:53 -0400

    Attempting to repair

--- adv.py
+++ adv.py
@@ -36,22 +36,24 @@ room_connections = {}
 
 # Setup Visited List
 rooms_to_visit = []
+rooms_visited = []
 paths_taken = []
+
 # Get first room
 current_room = player.current_room.id
 # Room Connections
 for each in player.current_room.get_exits():
     room_connections.update({each: "?"})
-print(room_connections)
 # Loop Thru Connections - returns 1 -> direction = next(key for key, value in room_connections.items() if value == "?")
 for direction, room in room_connections.items():
     if room == "?":
         rooms_to_visit.append([player.current_room.id, direction])
+# Add first connection to graph
+traversal_graph.update({player.current_room.id: room_connections})
 
 # List for Rooms Completed
-rooms_completed = []
-
-full_path = []
+# rooms_completed = []
+# full_path = []
 path_taken = []
 last_room_direction = None
 comment = ""
@@ -64,96 +66,83 @@ def reverseDirection(direction):
 
 # while the rooms_to_visit list is not Empty:
 while len(rooms_to_visit) > 0:
-
-    # full_path.append(player.current_room.id)
-    # path_taken.append(player.current_room.id)
-    print (f"Current Room: {player.current_room.id} ")
-    comment = f"Current Room: {player.current_room.id} "
-
-    if last_room_direction is not None:
-        # Remove Remove from que
-        if [current_room, last_room_direction] in rooms_to_visit:
-            rooms_to_visit.remove([current_room, last_room_direction])
-
-        #TODO: Remove Print 
-        print (f"Rooms to Visit: {rooms_to_visit}")
-
-    # Check if room is in the Traversal Graph
-    if not current_room in traversal_graph.keys():
-        # If not in graph, then check room and add to graph
-        room_connections = {}
-        # Add each connection as unknown
-        for direction in player.current_room.get_exits():
-            room_connections.update({direction: "?"})
-            if [current_room, direction] not in rooms_to_visit:
-                rooms_to_visit.append([current_room, direction])
-        # Add connections to graph
-        traversal_graph.update({player.current_room.id: room_connections})
+    current_room = player.current_room.id
+    
+    print (f"Current Room: {current_room} ")
+    comment = f"Comment - Current Room: {current_room} "
 
     # Check Connected Rooms in Graph
-    room_connections = traversal_graph.get(player.current_room.id)
-    #TODO: 
-    print (f"Current Room Connections: {room_connections}")
+    room_connections = traversal_graph.get(current_room)
+
+    # Update were came from
+    if last_room_direction is not None and last_room is not None:
+        new_direction = reverseDirection(last_room_direction)
+        room_connections[new_direction] = last_room
+        print(f"Add Last Room {last_room}: {room_connections}")
 
     # Count how many ?
     unknown_connections = list(room_connections.values()).count("?")
-            
-    # Set Last Room, before moving
-    last_room = player.current_room.id
+    comment += f"- Connections: {room_connections} - Unknown Connections: {unknown_connections}"
     
     if unknown_connections > 0:
-            
-        # Move to new Room
-        for key, value in room_connections.items():
-            if value == "?":
-                if unknown_connections > 1 and last_room_direction is not None:
-                    if (last_room_direction == "n" and key == "s") \
-                        or (last_room_direction == "e" and key == "w") \
-                        or (last_room_direction == "s" and key == "n") \
-                        or (last_room_direction == "w" and key == "e"):
-                        continue
-                    else:
-                        direction = key
-                else:
-                    direction = key
+        # Get direction
+        direction = next(key for key, value in room_connections.items() if value == "?")
+        
+        # Remove Current Room and direction from que
+        if [current_room, direction] in rooms_to_visit:
+            rooms_to_visit.remove([current_room, direction])
+        comment += f"- Remove Path: {[current_room, direction]} "
+        comment += f"- Rooms to Visit: {rooms_to_visit} "
 
-                if [current_room, key] not in rooms_to_visit:
-                    rooms_to_visit.append([current_room, direction])
-
-        #TODO: Remove Print 
-        print (f"Rooms to Visit: {rooms_to_visit}")
-
-        # Go to new direction if it is not from the old direction
-        traversal_path.append(direction)
+        # Move Player new direction
         player.travel(direction)
+        # Add Path to Traversal Path
+        traversal_path.append(direction)
+        # Set last room direction
         last_room_direction = direction
+        # Append to current path reference
         path_taken.append([current_room, direction])
+        # Set Last Room, to previous current room
+        last_room = current_room
         # Update Connection
-        room_connections[direction] = player.current_room.id
+        current_room = player.current_room.id
+        room_connections[direction] = current_room
+                
+        # Check if room is in the Traversal Graph
+        if not current_room in traversal_graph.keys():
+            # If not in graph, then check room and add to graph
+            room_connections = {}
+            # Add each connection as unknown
+            for direction in player.current_room.get_exits():
+                room_connections.update({direction: "?"})
+                if [current_room, direction] not in rooms_to_visit:
+                    rooms_to_visit.append([current_room, direction])
+            # Add connections to graph
+            traversal_graph.update({player.current_room.id: room_connections})
 
+        # TODO: remove print
         print (f"Graph: {traversal_graph}")
-                        
-    # More Than 0, we need to check room
-    #if unknown_connections == 1:
-        # Go to next Room
-        # direction = next(key for key, value in room_connections.items() if value == "?")
-        # Set Last Room, before moving
-        # last_room = player.current_room.id
-        # # Move to new Room
-        # traversal_path.append(direction)
-        # player.travel(direction)
-        # last_room_direction = direction
-        # # Update Connection
-        # room_connections[direction] = player.current_room.id
-        # if only 1 connection, this one is now complete
-        # rooms_completed.append(last_room)
-        # comment += f"- Room Complete: {last_room}"
-        # path_taken.remove(last_room)
 
-    # If 0 connections, then room completed
+
+    # If 0 connections, then room completed. Check path for incomplete
     else:
         comment += f"- Room Complete: {last_room}"
-        rooms_completed.append(last_room)
+        # Go to last room
+        last_path = path_taken.pop(-1)
+        print (f"Last Path {last_path}")
+        direction = last_path[1]
+        # Move Player new direction
+        player.travel(direction)
+        # Add Path to Traversal Path
+        traversal_path.append(direction)
+        # Set last room direction
+        last_room_direction = direction
+        # Set Last Room, to previous current room
+        last_room = current_room
+        # Update Connection
+        current_room = player.current_room.id
+        # room_connections[direction] = current_room
+
     # else:
     #     # If Room listed as completed, go back
     #     # if last_room in rooms_to_visit:
